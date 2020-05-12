5449c6dcf1ae2d0800f324c3044ddd3d13d65a3a
jawad727 <jawaddhussein@gmail.com>
2020-05-11 19:50:31 -0700

    first attempt, time limit exceeded. Using quadratic time

---
+++ JavaScript/Medium/threeSum.js
@@ -0,0 +1,21 @@
+var threeSum = function(nums) {
+    var arr = []
+    var obj = {}
+    nums = nums.sort()
+    
+    for (i=1; i<nums.length; i++) {
+        
+        obj[nums[i - 1]] = nums[i - 1]
+        
+        for (j=i+1; j<nums.length; j++) {
+          var pushArr = [obj[ 0 - (nums[i] + nums[j]) ], nums[i], nums[j]]
+            
+            if ( 0 - (nums[i] + nums[j]) in obj ) {
+   
+                    arr.push( pushArr )      
+            }  
+        }
+    }
+    
+    return arr.filter(( t={}, a=> !(t[a]=a in t)))  
+};
